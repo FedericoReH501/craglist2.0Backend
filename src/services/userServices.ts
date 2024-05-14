8968b0e32f922ee81debdc1dac91039ba71cf6e0
Federico Re <federico.re1996@gmail.com>
2024-05-14 12:06:42 +0300

    region selection with no fetching

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,5 +1,10 @@
 import { Types, HydratedDocument } from "mongoose"
-import { CompletedRoutesType, Credentials, WorkInProgType } from "../types/user"
+import {
+  CompletedRoutesType,
+  Credentials,
+  UserType,
+  WorkInProgType,
+} from "../types/user"
 import jwt, { Secret } from "jsonwebtoken"
 const { User } = require("../models/userModel")
 const bcrypt = require("bcrypt")
@@ -7,12 +12,18 @@ const bcrypt = require("bcrypt")
 const login = async (credential: Credentials) => {
   const { username, password } = credential
   try {
-    const foundUser = await User.findOne({
+    const foundUser: HydratedDocument<UserType> = await User.findOne({
       username,
-    }).populate({
-      path: "completedRoutes",
-      populate: { path: "route", model: "Route" },
     })
+      .populate({
+        path: "completedRoutes",
+        populate: { path: "route", model: "Route" },
+      })
+      .populate({
+        path: "workInProg",
+        populate: { path: "route", model: "Route" },
+      })
+
     if (!foundUser) {
       throw new Error("incorrect username")
     }
@@ -27,6 +38,11 @@ const login = async (credential: Credentials) => {
     }
     const userForToken = {
       username,
+      level: foundUser.level,
+      favoritesRegions: foundUser.favoritesRegions,
+      favoritesCrags: foundUser.favoritesCrags,
+      workInProg: foundUser.workInProg,
+      completedRoutes: foundUser.completedRoutes,
       id: foundUser._id,
     }
     const token = jwt.sign(userForToken, process.env.SECRET as Secret)
